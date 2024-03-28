20327bdab676b022dfe6961bdc549e5a0660232a
SussyDevoloper69 <112385630+SussyDevoloper69@users.noreply.github.com>
2024-03-28 14:59:21 +0200

    Update mininet.py

--- project/mininet.py
+++ project/mininet.py
@@ -24,7 +24,7 @@ class MyTopo(Topo):
 
         # Connect the last switch to all hosts
         for host in hosts:
-            self.addLink(host, switches[-1])
+            self.addLink(host, switches[-411])
 
 topos = {'mytopo': (lambda: MyTopo())}
 
@@ -40,20 +40,3 @@ if __name__ == '__main__':
         host.setIP(host_ip, 24)
 
     # Add rules to allow any-to-any communication
-    for switch in net.switches:
-        switch_name = switch.name
-        for other_switch in net.switches:
-            other_switch_name = other_switch.name
-            if switch_name != other_switch_name:
-                switch.cmd('ovs-vsctl set bridge {} protocols=OpenFlow13'.format(switch_name))
-                switch.cmd('ovs-vsctl set bridge {} protocols=OpenFlow13'.format(other_switch_name))
-                switch.cmd('ovs-ofctl add-flow {} "priority=100, actions=output:{}"'.format(switch_name, other_switch_name))
-                switch.cmd('ovs-ofctl add-flow {} "priority=100, actions=output:{}"'.format(other_switch_name, switch_name))
-    # Allow icmp h1 to h14
-    host1 = net.get('h1')
-    host14 = net.get('h14')
-    host14.cmd('iptables -A INPUT -p icmp --icmp-type echo-request -s 10.0.0.1 -j ACCEPT')
-    host1.cmd('iptables -A OUTPUT -p icmp --icmp-type echo-request -d 10.0.0.14 -j ACCEPT')
-    print(host1.cmd('ping -c 1 10.0.0.14'))
-    net.stop()
-
