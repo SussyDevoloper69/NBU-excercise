fea35f21abf38342a29275156b6acf39ead69ba2
SussyDevoloper69 <112385630+SussyDevoloper69@users.noreply.github.com>
2024-03-28 14:53:55 +0200

    Update automate.sh

--- project/automate.sh
+++ project/automate.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+curl http://localhost:8080/wm/firewall/module/enable/json -X PUT -d ''
+curl -X POST -d '{"src-ip": "10.0.0.1/32", "dst-ip": "10.0.0.14/32", "dl-type":"ARP" }' http://localhost:8080/wm/firewall/rules/json
+curl -X POST -d '{"src-ip": "10.0.0.14/32", "dst-ip": "10.0.0.1/32", "dl-type":"ARP" }' http://localhost:8080/wm/firewall/rules/json
+curl -X POST -d '{"src-ip": "10.0.0.1/32", "dst-ip": "10.0.0.14/32", "nw-proto":"ICMP" }' http://localhost:8080/wm/firewall/rules/json
+curl -X POST -d '{"src-ip": "10.0.0.14/32", "dst-ip": "10.0.0.1/32", "nw-proto":"ICMP" }' http://localhost:8080/wm/firewall/rules/json
 sudo docker exec -it project-mininet-1 bash -c 'mn --custom /scripts/mininet.py --topo mytopo --controller=remote,ip=floodlight,port=6653'
